5336510e5c5ab399d9ae7fe86b1459509192cd60
daikitsujimoto <tsujimoto0427@gmail.com>
2020-08-02 21:41:07 +0900

    4-3~5-4  commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,12 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: `AYALA`,
+    description: `AYALAのYOGA情報発信&レッスン予約サイト`,
+    lang: "ja",
+    siteUrl: `https://keen-newton-816efa.netlify.app`,
+  },
   plugins: [
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -16,5 +22,6 @@ module.exports = {
         path: `${__dirname}/src/images/`,
       },
     },
+    `gatsby-plugin-react-helmet`,
   ],
 }
---
+++ src/components/seo.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import { graphql, useStaticQuery } from "gatsby"
+
+export default props => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          lang
+          description
+          siteUrl
+        }
+      }
+    }
+  `)
+
+  const title = props.pagetitle
+    ? `${props.pagetitle} | ${data.site.siteMetadata.title}`
+    : data.site.siteMetadata.title
+  const description = props.pagedesc || data.site.siteMetadata.description
+  const url = props.pagepath
+    ? `${data.site.siteMetadata.siteUrl}${props.pagepath}`
+    : data.site.siteMetadata.siteUrl
+  console.log(props)
+
+  return (
+    <Helmet>
+      <html lang={data.site.siteMetadata.lang} />
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <link rel="canonical" href={url} />
+    </Helmet>
+  )
+}
