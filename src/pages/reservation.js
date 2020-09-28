87c43d850314ec8fb779e548af16a2ffe8363abe
daikitsujimoto <tsujimoto0427@gmail.com>
2020-09-29 01:46:03 +0900

    予約フォーム

--- src/pages/reservation.js
+++ src/pages/reservation.js
@@ -16,14 +16,41 @@ export default ({ data, location }) => {
       <div>
         <article className="content">
           <div className="container">
-            <h1 className="bar">SCHEDULE</h1>
+            <h1 className="bar mb-0">レッスンご予約</h1>
+            <div className="postbody">
+              <p className="mb-0">
+                こちらからの返信を持ちまして申込み完了となります。
+                <br />
+                1週間以内に返信が無い場合は、下記アドレスまでご連絡下さいませ。
+              </p>
+              <p>ayazonyoga@gmail.com</p>
+            </div>
+
             <form name="contact" method="post" data-netlify="true">
               <input type="hidden" name="form-name" value="contact" />
-              <p>
-                <label>
-                  Your Name: <input type="text" name="name" />
-                </label>
-              </p>
+              <div>
+                <p>開催日程</p>
+                <div className="checkbox-bk">
+                  <div>
+                    <input
+                      type="checkbox"
+                      id="coding"
+                      name="interest"
+                      value="coding"
+                    />
+                    <label for="coding">コーディング</label>
+                  </div>
+                  <div>
+                    <input
+                      type="checkbox"
+                      id="music"
+                      name="interest"
+                      value="music"
+                    />
+                    <label for="music">音楽</label>
+                  </div>
+                </div>
+              </div>
               <p>
                 <label>
                   Your Email: <input type="email" name="email" />
